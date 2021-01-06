55bb912009e5f6aea67d0a081f981765c6616ef5
biancaty7i <6687190@gmail.com>
2021-01-06 13:37:54 +0800

    add types/gateway_types migration

---
+++ database/migrations/2021_01_06_133227_create_gateway_types_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGatewayTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gateway_types', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('gateways_id');
+            $table->integer('types_id');
+            $table->boolean('is_support_deposit');
+            $table->boolean('is_support_withdraw');
+            $table->timestamps();
+            $table->unique(['gateways_id','types_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gateway_types');
+    }
+}
