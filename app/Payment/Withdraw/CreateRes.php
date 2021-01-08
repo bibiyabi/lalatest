8116bf788df9230c4cbb9c21db336c2b907128ff
colin <6687195@gmail.com>
2021-01-08 16:41:52 +0800

    res fix

--- app/Payment/Withdraw/CreateRes.php
+++ app/Payment/Withdraw/CreateRes.php
@@ -8,15 +8,15 @@ trait CreateRes
     private $failed = 4;
 
     public function resSuccess($data = []) {
-        return ['code'=> $this->success, $msg=>'', 'data' => $data];
+        return ['code'=> $this->success, 'msg'=>'', 'data' => $data];
     }
 
     public function resRetry($data = []) {
-        return ['code'=> $this->success, $msg=>'', 'data' => $data];
+        return ['code'=> $this->success, 'msg'=>'', 'data' => $data];
     }
 
     public function resFailed($data = []) {
-        return ['code'=> $this->success, $msg=>'','data' => $data];
+        return ['code'=> $this->success, 'msg'=>'','data' => $data];
     }
 
 
