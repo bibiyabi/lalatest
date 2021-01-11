3f0b44ce65b565a69989099b3ab119ec2cc95327
alanx15a2 <6687170@gmail.com>
2021-01-11 17:05:21 +0800

    新增 DepositRepository

---
+++ app/Repositories/Orders/DepositRepository.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Repositories\Orders;
+
+use App\Models\Order;
+use App\Contracts\Payments\Status;
+use Illuminate\Http\Request;
+
+class DepositRepository
+{
+    public function __construct(Order $order) {
+        $this->order = $order;
+    }
+
+    public function create(Request $request, $gateway_id): Order
+    {
+        $user = $request->user();
+        $order_param = $request->post();
+        unset($order_param['order_id'], $order_param['key_id'], $order_param['amount']);
+
+        return Order::create([
+            'order_id' => $request->post('order_id'),
+            'user_id'  => $user->id,
+            'key_id'   => $request->post('key_id'),
+            'amount'   => $request->post('amount'),
+            'gateway_id' => $gateway_id,
+            'status'   => Status::PENDING,
+            'order_param' => json_encode($order_param),
+        ]);
+    }
+}
