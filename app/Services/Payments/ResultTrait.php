950e3bbda173915efd6c0ebad342b8db7e8964f4
colin <6687195@gmail.com>
2021-01-12 16:16:40 +0800

    與充值同狀態

--- app/Services/Payments/ResultTrait.php
+++ app/Services/Payments/ResultTrait.php
@@ -1,18 +1,17 @@
 <?php
 namespace App\Services\Payments;
 
-use App\Constants\WithDrawOrderStatus;
+use App\Contracts\Payments\Status;
 
 trait ResultTrait
 {
-    private $createSuccess = WithDrawOrderStatus::CREATE_SUCCESS;
-    private $createTimeout = WithDrawOrderStatus::CREATE_TIMEOUT;
-    private $createFailed = WithDrawOrderStatus::CREATE_FAIL;
-    private $createRetry = WithDrawOrderStatus::CREATE_RETRY;
+    private $createSuccess = Status::ORDER_SUCCES;
+    private $createTimeout = Status::ORDER_ERROR;
+    private $createFailed = Status::ORDER_FAILED;
+    private $createRetry = Status::ORDER_ERROR;
 
-    private $callbackSuccess = WithDrawOrderStatus::CALLBACK_SUCCESS;
-    private $callbackFail = WithDrawOrderStatus::CALLBACK_FAIL;
-    private $callbackRetry = WithDrawOrderStatus::CALLBACK_RETRY;
+    private $callbackSuccess = Status::CALLBACK_SUCCESS;
+    private $callbackFail = Status::CALLBACK_FAILED;
 
     public function resCreateSuccess($msg ='', $data = []) {
         return collect(['code'=> $this->createSuccess, 'msg'=>'', 'data' => $data]);
@@ -30,17 +29,12 @@ trait ResultTrait
         return collect(['code'=> $this->createRetry, 'msg'=>'','data' => $data]);
     }
 
-
     public function resCallbackSuccess($msg ='', $data = []) {
         return collect(['code'=> $this->callbackSuccess, 'msg'=>'', 'data' => $data]);
     }
 
     public function resCallbackFailed($msg ='',$data = []) {
         return collect(['code'=> $this->callbackFail, 'msg'=>'', 'data' => $data]);
-    }
-
-    public function resCallbackRetry($msg ='',$data = []) {
-        return collect(['code'=> $this->callbackRetry, 'msg'=>'','data' => $data]);
     }
 
 
