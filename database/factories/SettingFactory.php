b004ee23f371d9a412d91c4c4992ec3447b4780f
alanx15a2 <6687170@gmail.com>
2021-01-14 13:17:49 +0800

    rename key to setting

--- database/factories/SettingFactory.php
+++ database/factories/SettingFactory.php
@@ -2,10 +2,10 @@
 
 namespace Database\Factories;
 
-use App\Models\Keys;
+use App\Models\Setting;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class KeysFactory extends Factory
+class SettingFactory extends Factory
 {
     /**
      * The name of the factory's corresponding model.
