06624f345a431cdfa2c9e494f8f27f028a2252cb
colin <6687195@gmail.com>
2021-01-15 15:18:10 +0800

    拿掉注解

--- app/Http/Controllers/Payment/WithdrawOrderController.php
+++ app/Http/Controllers/Payment/WithdrawOrderController.php
@@ -17,31 +17,7 @@ class WithdrawOrderController extends Controller
     public function create(Request $request, PaymentInterface $payment) {
 
         $this->request = $request->post();
-        /*
-        $this->request = [];
-        $this->request['payment_type'] = 2; // 1 銀行卡 2 電子錢包 3 數字貨幣
-        $this->request['user_pk'] = 876;
-        $this->request['user_id'] = 1; // 可以先寫死
-        $this->request['merchant_name'] = 'java'; // 可以先寫死, 前端要先傳
-        $this->request['rate_amount'] = 10;
-
-        $this->request['order_id'] = 123456;
-        $this->request['payment_address'] = 'eoifjeoijfoie';
-        $this->request['user_phone'] = '123456';
-
-        // 銀行卡
-        //bankAddress 所在地区名称
-        //bankAddress
-
-        $this->request['bank_user_name'] = 'colin';
-        $this->request['bank_code'] = 123456;
-        $this->request['phone_number'] = 123456;
-        $this->request['user_email'] = 123456; // 用戶郵箱
-        $this->request['bank_ifsc'] = ''; // 分行代碼
-        */
-
         Log::channel('withdraw')->info(__LINE__ , $request->post());
-
 
         $this->request['user_id'] = $request->user()->id;
         $payment->checkInputData($this->request)->createToQueue();
