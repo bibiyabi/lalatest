858639a7fef6bf1febb114135ae68c62def43ed5
biancaty7i <6687190@gmail.com>
2021-01-20 14:59:53 +0800

    給JAVA測試

--- app/Services/Payments/WithdrawGateways/ShineUPay.php
+++ app/Services/Payments/WithdrawGateways/ShineUPay.php
@@ -163,7 +163,7 @@ class ShineUPay extends AbstractWithdrawGateway
     public function getPlaceholder($type):Placeholder
     {
         return new Placeholder($type, '', '','請填上md5密鑰','http://商戶後台/recharge/notify',
-        '');
+        '',['好好吃銀行','QQ銀行']);
     }
 
 
