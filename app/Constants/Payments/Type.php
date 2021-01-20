b4897ca33053718595ed4d278efad8cbd07ac17c
biancaty7i <6687190@gmail.com>
2021-01-20 11:05:19 +0800

    verify type constant

--- app/Constants/Payments/Type.php
+++ app/Constants/Payments/Type.php
@@ -4,6 +4,11 @@ namespace App\Constants\Payments;
 
 class Type
 {
+    public const CREDIT_CARD        = 'credit_card';
+    public const BANK_CARD          = 'bank_card';
+    public const WALLET             = 'e_wallet';
+    public const CRYPTO_CURRENCY    = 'cryptocurrency';
+
     const type = [
         'credit_card'       => 1,
         'bank_card'         => 2,
@@ -12,10 +17,10 @@ class Type
         ];
 
     const typeName = [
-        1 => 'credit_card',
-        2 => 'bank_card',
-        3 => 'e_wallet',
-        4 => 'cryptocurrency',
+        1 => self::CREDIT_CARD,
+        2 => self::BANK_CARD,
+        3 => self::WALLET,
+        4 => self::CRYPTO_CURRENCY,
     ];
 
 }
