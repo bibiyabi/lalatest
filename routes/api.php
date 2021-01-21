a41d7a64fa80a4b04f9b149af4be0604190e6b5c
alanx15a2 <6687170@gmail.com>
2021-01-21 17:07:45 +0800

    callback 移動至 no_auth route

--- routes/api.php
+++ routes/api.php
@@ -198,7 +198,6 @@ Route::post('aaa',function (Request $request)
 Route::prefix('withdraw')->group(function ()
 {
     Route::post('create', [WithdrawController::class, 'create']);
-    Route::post('callback/{gatewayName}', [WithdrawController::class, 'callback']);
     Route::post('reset', [WithdrawController::class, 'reset']);
 });
 
@@ -206,7 +205,6 @@ Route::prefix('withdraw')->group(function ()
 Route::prefix('deposit')->group(function ()
 {
     Route::post('create', [DepositController::class, 'create']);
-    Route::match(['get', 'post'], 'callback/{gatewayName}', [DepositController::class, 'callback']);
     Route::post('reset', [DepositController::class, 'reset']);
 });
 
---
+++ routes/no_auth.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Payment\DepositController;
+use App\Http\Controllers\Payment\WithdrawController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::match(['get', 'post'], 'withdraw/{gatewayName}', [WithdrawController::class, 'callback']);
+Route::match(['get', 'post'], 'deposit/{gatewayName}', [DepositController::class, 'callback']);
