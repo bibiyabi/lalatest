609fdc40b1a1771cd74ee254dc4896c4455a60bf
biancaty7i <6687190@gmail.com>
2021-01-28 17:39:19 +0800

    add test, verify doc. V11

---
+++ tests/Feature/PlaceholderTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Merchant;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Constants\Payments\Type;
+
+class PlaceholderTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    private $user;
+
+//    public function setUp(): void
+//    {
+//        parent::setUp();
+//
+//        $user = Merchant::factory([
+//            'name' => 'java',
+//        ])->create();
+//
+//        $this->user = $user;
+//        $this->actingAs($user);
+//    }
+
+    public function testGatewayList()
+    {
+        $this->withoutMiddleware();
+
+        $response = $this->call('get','api/vendor/list',[
+            'is_deposit' => 1,
+            'type'=> 'e_wallet'
+        ]);
+
+        $response->assertJsonFragment(['success'=> true]);
+
+        $this->assertDatabaseHas('gateway_types',[
+            'types_id' => Type::type['e_wallet'],
+            'is_support_deposit' => 1
+        ]);
+    }
+
+    public function testPlaceholder()
+    {
+        $this->withoutMiddleware();
+
+        $response = $this->call('get','api/placeholder',[
+            'is_deposit' => 0,
+            'type'=> 'bank_card',
+            'gateway_name'=> 'ShineUPay'
+        ]);
+
+        $response->assertJsonFragment(['success'=> true]);
+    }
+
+    public function testRequirement()
+    {
+        $this->withoutMiddleware();
+
+        $response = $this->call('get','api/requirement',[
+            'is_deposit' => 0,
+            'type'=> 'bank_card',
+            'gateway_name'=> 'ShineUPay'
+        ]);
+
+        $response->assertJsonFragment(['success'=> true]);
+    }
+}
