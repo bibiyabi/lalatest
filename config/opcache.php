a7d9c96523ffaeaac47de5aecb37e0e9a022bb1c
alanx15a2 <6687170@gamil.com>
2021-02-03 11:00:19 +0800

    add laravel opcache

---
+++ config/opcache.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'url' => env('OPCACHE_URL', config('app.url')),
+    'prefix' => 'api/opcache/',
+    'verify' => true,
+    'headers' => [],
+    'directories' => [
+        base_path('app'),
+        base_path('bootstrap'),
+        base_path('public'),
+        base_path('resources'),
+        base_path('routes'),
+        base_path('storage'),
+        base_path('vendor'),
+    ],
+    'exclude' => [
+        'test',
+        'Test',
+        'tests',
+        'Tests',
+        'stub',
+        'Stub',
+        'stubs',
+        'Stubs',
+        'dumper',
+        'Dumper',
+        'Autoload',
+        'symfony/polyfill-ctype',
+        'symfony/polyfill-iconv',
+        'symfony/polyfill-intl-grapheme',
+        'symfony/polyfill-intl-idn',
+        'symfony/polyfill-intl-normalizer',
+        'symfony/polyfill-mbstring',
+        'symfony/polyfill-php80',
+    ],
+];
