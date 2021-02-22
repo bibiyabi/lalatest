ef5dfaa051db01412adb7b87c9f9b3b4c534f48e
alanx15a2 <6687170@gamil.com>
2021-02-22 14:43:25 +0800

    新增假第三方介面

---
+++ app/Http/Controllers/FakeDepositController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Constants\Payments\Status;
+use App\Exceptions\NotifyException;
+use App\Models\Order;
+use App\Services\Payments\Deposit\DepositNotify;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+
+class FakeDepositController extends Controller
+{
+    private $service;
+
+    public function __construct(DepositNotify $service) {
+        $this->service = $service;
+    }
+
+    public function index()
+    {
+        $minDate = Carbon::now()->subDays(5);
+        $orders = Order::where('created_at', '>=', $minDate)->get();
+
+        return view('fake_tparty.deposit', compact('orders'));
+    }
+
+    public function sendNotify(Order $order, Request $request)
+    {
+        $validated = $request->validate([
+            'success' => 'required|boolean'
+        ]);
+
+        $order->status = $validated['success']
+            ? Status::CALLBACK_SUCCESS
+            : Status::CALLBACK_FAILED;
+
+        try {
+            $this->service->notify($order);
+        } catch (NotifyException $e) {
+            return response(['success'=>false]);
+        }
+
+        return response(['success'=>true]);
+    }
+}
---
+++ resources/views/bootstrap.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    @yield('header')
+</head>
+<body>
+    @yield('content')
+    @yield('script')
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+</body>
+</html>
