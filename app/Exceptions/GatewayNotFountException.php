e232cc1e153be26cd2e27eb6f1ffe45bb02c68f6
biaoncaty7i <6687190@gmail.com>
2021-02-25 15:16:28 +0800

    修改錯誤response code

--- app/Exceptions/GatewayNotFountException.php
+++ app/Exceptions/GatewayNotFountException.php
@@ -2,9 +2,20 @@
 
 namespace App\Exceptions;
 
+use App\Constants\Payments\ResponseCode;
 use Exception;
+use Illuminate\Support\Facades\Log;
+use MarcinOrlowski\ResponseBuilder\ResponseBuilder as RB;
 
 class GatewayNotFountException extends Exception
 {
-    //
+    public function report()
+    {
+        Log::info('Gateway Not Found Exception');
+    }
+
+    public function render()
+    {
+        return RB::error(ResponseCode::ERROR_DATA_IN_PAYMENT);
+    }
 }
