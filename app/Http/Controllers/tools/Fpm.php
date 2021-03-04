5666e9e167bdb26c92b5d9430484badf180dfcf3
colin <6687195@gmail.com>
2021-03-04 11:30:28 +0800

    fpm fix

--- app/Http/Controllers/tools/Fpm.php
+++ app/Http/Controllers/tools/Fpm.php
@@ -17,7 +17,7 @@ class Fpm extends Controller
         if (config('app.env') !== 'local'){
             $file = file('/home/pmuser/colin/fpm.log');
         } else {
-            die('please put fpm.log to some where');
+            $file = file(dirname(__FILE__) . '/fpm.log');
         }
         $array = '';
 
@@ -31,13 +31,16 @@ class Fpm extends Controller
         $maxactiveprocesses = '';
         $maxchildrenreached = '';
         $slowrequests = '';
-
+        $startsince = '';
         $hostname = '';
+
         foreach( $file as $lineNum => $lineData ) {
             if (empty($lineData)) continue;
             $data = json_decode($lineData, true);
             $hostname = $data['hostname'];
+            $data['time'] = date("Y-m-d H:i:s", strtotime($data['time'] . " +8 hours"));
             $acceptedconn .= '{x:"'.$data['time'].'", y:'.$data['acceptedconn'].'},';
+            $startsince .= '{x:"'.$data['time'].'", y:'.$data['startsince'].'},';
             $listenqueue .= '{x:"'.$data['time'].'", y:'.$data['listenqueue'].'},';
             $maxlistenqueue .= '{x:"'.$data['time'].'", y:'.$data['maxlistenqueue'].'},';
             $listenqueuelen .= '{x:"'.$data['time'].'", y:'.$data['listenqueuelen'].'},';
@@ -47,25 +50,22 @@ class Fpm extends Controller
             $maxactiveprocesses .= '{x:"'.$data['time'].'", y:'.$data['maxactiveprocesses'].'},';
             $maxchildrenreached .= '{x:"'.$data['time'].'", y:'.$data['maxchildrenreached'].'},';
             $slowrequests .= '{x:"'.$data['time'].'", y:'.$data['slowrequests'].'},';
-
         }
 
-         $acceptedconn = substr( $acceptedconn, 0, -1);
-         $listenqueue = substr( $listenqueue, 0, -1);
-
-         $maxlistenqueue = substr( $maxlistenqueue, 0, -1);
-
-          $listenqueuelen = substr( $listenqueuelen, 0, -1);
-
-          $idleprocesses = substr( $idleprocesses, 0, -1);
-          $activeprocesses = substr( $activeprocesses, 0, -1);
-
-          $totalprocesses = substr( $totalprocesses, 0, -1);
-          $maxactiveprocesses = substr( $maxactiveprocesses, 0, -1);
-          $maxchildrenreached = substr( $maxchildrenreached, 0, -1);
-          $slowrequests = substr( $slowrequests, 0, -1);
+        $startsince = substr( $startsince, 0, -1);
+        $acceptedconn = substr( $acceptedconn, 0, -1);
+        $listenqueue = substr( $listenqueue, 0, -1);
+        $maxlistenqueue = substr( $maxlistenqueue, 0, -1);
+        $listenqueuelen = substr( $listenqueuelen, 0, -1);
+        $idleprocesses = substr( $idleprocesses, 0, -1);
+        $activeprocesses = substr( $activeprocesses, 0, -1);
+        $totalprocesses = substr( $totalprocesses, 0, -1);
+        $maxactiveprocesses = substr( $maxactiveprocesses, 0, -1);
+        $maxchildrenreached = substr( $maxchildrenreached, 0, -1);
+        $slowrequests = substr( $slowrequests, 0, -1);
 
         return view('tools.fpm' ,compact('acceptedconn',
+        'startsince',
         'listenqueue' ,
         'maxlistenqueue',
         'listenqueuelen',
@@ -74,7 +74,9 @@ class Fpm extends Controller
         'maxactiveprocesses',
         'maxchildrenreached',
         'slowrequests',
-        'hostname'
+        'hostname',
+        'idleprocesses',
+        'activeprocesses',
 
     ));
     }
