bd9efa2e0a0c445964c67666c48b089271c19fd9
colin <6687195@gmail.com>
2021-03-15 13:59:51 +0800

    javaapi test

--- tests/Unit/Http/Middleware/JavaApiKeyTest.php
+++ tests/Unit/Http/Middleware/JavaApiKeyTest.php
@@ -2,8 +2,14 @@
 
 namespace Tests\Unit\Http\Middleware;
 
+use App\Http\Middleware\JavaApiKey;
+use App\Lib\Hash\Signature;
+use App\Models\Merchant;
+use App\Repositories\MerchantRepository;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Request;
+use Mockery\MockInterface;
 use Tests\TestCase;
 
 class JavaApiKeyTest extends TestCase
@@ -14,6 +20,88 @@ class JavaApiKeyTest extends TestCase
     public function setUp():void
     {
         parent::setUp();
+    }
+
+    public function test_handle_same_sign() {
+        $user = Merchant::factory([
+            'name' => 'java',
+        ])->create();
+
+        $this->actingAs($user);
+
+        Config::set('app.is_check_sign', true);
+
+        $request = Request::create('/api/withdraw/create', 'POST', ['sign' => 'aaa']);
+
+        $mockMerchantRepo = $this->mock(MerchantRepository::class, function (MockInterface $mock) {
+            $mock->shouldReceive('getKey')->once()->andReturn('');
+        });
+
+        $mockSignature = $this->mock(Signature::class, function (MockInterface $mock) {
+            $mock->shouldReceive('makeSign')->once()->andReturn('aaa');
+        });
+
+        $middleware = new JavaApiKey($mockMerchantRepo, $mockSignature);
+
+        $response = $middleware->handle($request, function () {return 'next';});
+
+        $this->assertEquals($response, 'next');
+
+
+    }
+
+
+    public function test_handle_different_sign() {
+        $user = Merchant::factory([
+            'name' => 'java',
+        ])->create();
+
+        $this->actingAs($user);
+
+        Config::set('app.is_check_sign', true);
+
+        $request = Request::create('/api/withdraw/create', 'POST', ['sign' => 'ccc']);
+
+        $mockMerchantRepo = $this->mock(MerchantRepository::class, function (MockInterface $mock) {
+            $mock->shouldReceive('getKey')->once()->andReturn('');
+        });
+
+        $mockSignature = $this->mock(Signature::class, function (MockInterface $mock) {
+            $mock->shouldReceive('makeSign')->once()->andReturn('aaa');
+        });
+
+        $middleware = new JavaApiKey($mockMerchantRepo, $mockSignature);
+
+        $response = $middleware->handle($request, function () {return 'next';});
+        $this->assertEquals($response->original['code'], 159);
+
+    }
+
+
+    public function test_handle_success_when_is_check_sign_config_is_false() {
+        $user = Merchant::factory([
+            'name' => 'java',
+        ])->create();
+
+        $this->actingAs($user);
+
+        Config::set('app.is_check_sign', false);
+
+        $request = Request::create('/api/withdraw/create', 'POST', ['sign' => 'ccc']);
+
+        $mockMerchantRepo = $this->mock(MerchantRepository::class, function (MockInterface $mock) {
+            $mock->shouldReceive('getKey')->once()->andReturn('');
+        });
+
+        $mockSignature = $this->mock(Signature::class, function (MockInterface $mock) {
+            $mock->shouldReceive('makeSign')->once()->andReturn('aaa');
+        });
+
+        $middleware = new JavaApiKey($mockMerchantRepo, $mockSignature);
+
+        $response = $middleware->handle($request, function () {return 'next';});
+        $this->assertEquals($response, 'next');
+
     }
 
 
